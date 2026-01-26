f42773e51059737be48b8b01f2c4a27f9bf89be1
Valery Ponomareva <vale.ponick@gmail.com>
2026-01-26 23:58:37 +0700

    feat: solved tasks

---
+++ Swift?NotQuestion/Basics/basics.swift
@@ -0,0 +1,10 @@
+//
+//  basics.swift
+//  Swift?NotQuestion
+//
+//  Created by Валерия Пономарева on 26.01.2026.
+//
+
+import Foundation
+
+/* "Task 1: выведи имя юзера и выведи сообщение: 'Hi, [name]!'") */
